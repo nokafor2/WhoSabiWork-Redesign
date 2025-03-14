b3eb8a39e4795e367e197718bbbc4b351ba6ecc1
Nna-ayua Okafor <nna_okafor@yahoo.com>
2025-03-14 11:22:10 +0000

    Updated SearchEntrepreneur.vue

--- resources/js/components/UI/SearchEntrepreneur.vue
+++ resources/js/components/UI/SearchEntrepreneur.vue
@@ -6,13 +6,53 @@
             </div>
             <button class="btn btn-sm btn-outline-success" type="submit">Search</button>
         </form> -->
-        <form action="" class="row justify-content-center">
+        <form class="row justify-content-center" @submit.prevent="submitForm">
             <div class="col-12 col-sm-6 col-md-6 align-items-center">
                 <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Search an entrepreneur" aria-label="Search an entrepreneur" aria-describedby="button-addon2">
-                    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
+                    <input type="text" class="form-control" placeholder="Search an entrepreneur" aria-label="Search an entrepreneur" aria-describedby="button-addon2" v-model.trim="searchVal"  @input="sendSearchVal">
+                    <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Search</button>
                 </div>
             </div>
         </form>
     </section>
 </template>
+
+<script>
+    // import functions for use
+    import { reactive } from 'vue';
+    // import { route } from 'Ziggy';
+    import { router, useForm, usePage } from '@inertiajs/vue3';
+
+    export default {
+        data() {
+            return {
+                searchVal: ""
+            }
+        },
+        methods: {
+            // sendSearchVal() {
+            //     console.log(this.searchVal);
+            // },
+            async submitForm() {
+                const formData = reactive({
+                    searchVal: this.searchVal,
+                });
+
+                router.post('api/search', formData, {
+                    preserveState: true, // Prevents a full page reload
+                    onSuccess: (page) => {
+                        console.log(page);
+                    },
+                    onError: (errors) => {
+                        console.log('Error: ', errors);
+                    }
+                });
+            }
+        },
+        watch: {
+            searchVal() {
+                console.log(this.searchVal);
+            }
+        }
+    }
+</script>
