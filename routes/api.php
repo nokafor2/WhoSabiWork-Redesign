4857d4b4cc466b80d50913aaf7765ec45bf6c394
Nna-ayua Okafor <nna_okafor@yahoo.com>
2025-03-14 11:25:26 +0000

    Updated api.php

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,14 @@
 <?php
 
 use App\Http\Controllers\Api\ArtisanController;
+use App\Http\Controllers\Api\SearchController;
+use App\Http\Controllers\Api\SparePartController;
 use App\Http\Controllers\Api\StateController;
+use App\Http\Controllers\Api\TechnicalServiceController;
 use App\Http\Controllers\Api\TownController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\VehicleBrandController;
+use App\Http\Controllers\Api\VehicleCategoryController;
 use Doctrine\DBAL\Schema\Index;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,8 +28,13 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-Route::apiResource('artisans', ArtisanController::class);
-Route::apiResource('users', UserController::class);
+Route::apiResource('artisan', ArtisanController::class);
+Route::apiResource('user', UserController::class);
 Route::apiResource('states', StateController::class)->only('store');
 Route::apiResource('towns', TownController::class)->only('store');
+Route::apiResource('technicalServices', TechnicalServiceController::class)->only('store');
+Route::apiResource('spareParts', SparePartController::class)->only('store');
+Route::apiResource('vehicleCategories', VehicleCategoryController::class)->only('store');
+Route::apiResource('vehicleBrands', VehicleBrandController::class)->only('store');
+Route::apiResource('search', SearchController::class)->only('store');
 // Route::get('/artisans', [ArtisanController::class, index()]);
