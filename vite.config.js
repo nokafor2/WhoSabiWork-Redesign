b9e67b064a582a31838c334a748e47a4606fdd75
Nna-ayua Okafor <nna_okafor@yahoo.com>
2025-03-14 11:26:58 +0000

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import path from 'path'
 
 export default defineConfig({
     plugins: [
@@ -30,4 +31,10 @@ export default defineConfig({
             },
         }),
     ],
+    resolve: {
+        alias: {
+            // ziggy: path.resolve(... paths: 'vendor/tightenco/ziggy/dist/vue.es.js')
+            ziggy: path.resolve('vendor/tightenco/ziggy/dist/index.esm.js')
+        }
+    }
 });
