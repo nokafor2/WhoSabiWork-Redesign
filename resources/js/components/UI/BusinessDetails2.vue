c776194d7596e70a6befe543b4f3c65abee7c2a9
Nna-ayua Okafor <nna_okafor@yahoo.com>
2025-03-19 11:57:55 +0000

    Created BusinessDetails2.vue

---
+++ resources/js/components/UI/BusinessDetails2.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="col-sm-5 col-11 mt-3 mt-sm-3 shadow rounded text-light" style="background-color: #040D14">
+        <div class="row mt-2">
+            <div class="row">
+                <p class="col-auto mb-0">Business Category:</p>
+                <ul class="col list-inline">
+                    <li class="list-inline-item" v-for="(category, index) in user.userCategories" :key="index">{{ showCategory(index) }},</li>
+                </ul>
+            </div>
+            <div class="row" v-for="(category, index) in user.userCategories" :key="index">
+                <p class="col-auto mb-0">{{ categoryHeading(index) }}</p>
+                <ul class="list-inline">
+                    <li class="list-inline-item" v-for="(type, id) in category" :key="id">{{ type }},</li>
+                </ul>
+            </div>
+            <div class="row">
+                <p class="col-auto mb-0">Description</p>
+                <p>
+                    {{ businessDescription }}
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['user'],
+        data() {
+            return {
+                artisans: [],
+                products: [],
+                spareParts: [],
+                technicalServices: []
+            }
+        },
+        methods: {
+            showCategory(category) {
+                category = this.capitalizeFirstLetter(category);
+                return category.replace(/_/g, " ");
+            },
+            capitalizeFirstLetter(str) {
+                return str.charAt(0).toUpperCase() + str.slice(1);
+            },
+            categoryHeading(category) {
+                if (category === 'artisan') {
+                    return 'Artisan Services:';
+                } else if (category === 'mobile_marketer') {
+                    return 'Products:';
+                } else if (category === 'mechanic') {
+                    return 'Technical Services:';
+                } else if (category === 'spare_part_seller') {
+                    return 'Spare Part Products:';
+                }
+            }
+        },
+        computed: {
+            businessDescription() {
+                return this.user.userDetails.business_category.business_description;
+            },
+            businessCategory() {
+                if (this.user.userCategories.artisan?.length) {
+                    this.artisans = this.user.userCategories.artisan;
+                }
+                if (this.user.userCategories.product?.length) {
+                    this.products = this.user.userCategories.mobile_marketer;
+                }
+                if (this.user.userCategories.spare_part?.length) {
+                    this.spareParts = this.user.userCategories.spare_part_seller;
+                }
+                if (this.user.userCategories.technical_service?.length) {
+                    this.technicalServices = this.user.userCategories.mechanic;
+                }
+
+
+
+                return "";
+            }
+        }
+    }
+</script>
