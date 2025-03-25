0f36ede80ca39c19bf6644b182ceb9e49a914b46
Nna-ayua Okafor <nna_okafor@yahoo.com>
2025-03-25 07:32:43 +0000

    Created BusinessCategoryController.php

---
+++ app/Http/Controllers/BusinessCategoryController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Traits\GlobalFunctions;
+use App\Models\BusinessCategory;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class BusinessCategoryController extends Controller
+{
+    use GlobalFunctions;
+    
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+        $businessCategory = BusinessCategory::where('user_id', '=', $user->id)->first();
+        // dd($businessCategory);
+        $updateVal = $request->updateVal;
+        $updateField = $request->updateField;
+        $businessCategory->{$updateField} = $updateVal;
+        $result = $businessCategory->save();
+
+        return redirect()->route('users.show', $id)->with('success', $result);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
