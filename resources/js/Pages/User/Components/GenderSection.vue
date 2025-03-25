fccac6197daf648f3efb24700c5c0fca8678a564
Nna-ayua Okafor <nna_okafor@yahoo.com>
2025-03-25 07:27:01 +0000

    Updated GenderSection.vue

--- resources/js/Pages/User/Components/GenderSection.vue
+++ resources/js/Pages/User/Components/GenderSection.vue
@@ -4,27 +4,19 @@
             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
                 <dl class="row col-12 mb-0">
                     <dt class="col-sm-3">Gender:</dt>
-                    <dd class="col-sm-9 mb-0">Male</dd>
+                    <dd class="col-sm-9 mb-0">{{ getGender }}</dd>
                 </dl>
             </button>
         </h2>
         <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour">
             <div class="accordion-body">
-                <form class=""  action="{{ route('register') }}" method="POST">
-                    <!-- @csrf -->
+                <form class="" @submit.prevent="submitAction">
                     <div class="form-group col-12 mb-3">
-                        <select name="gender" id="gender" required
-                        class="form-control {{ $errors->has('gender') ? 'is-invalid' : '' }}">
+                        <select name="gender" id="gender" class="form-control" v-model="genderInput">
                             <option value="">Select Gender</option>
                             <option value="male">Male</option>
                             <option value="female">Female</option>
                         </select>
-            
-                        <!-- @if ($errors->has('gender'))
-                            <span class="invalid-feedback">
-                                <strong>{{ $errors->first('gender') }}</strong>
-                            </span>
-                        @endif -->
                     </div>
                     
                     <div class="row justify-content-between">
@@ -39,16 +31,41 @@
 
 
 <script>
+    import MethodsMixin from '../Mixins/MethodsMixin.js';
+    import { useForm } from '@inertiajs/vue3';
+
     export default {
-        props: [],
+        mixins: [MethodsMixin],
+        props: ['gender', 'userId'],
         emits: [],
         data() {
             return {
-
+                genderInput: ''
             }
         },
         methods: {
-            
+            submitAction() {
+                console.log(this.genderInput);
+                const dataToSend = useForm({
+                    updateVal: this.genderInput,
+                    updateField: 'gender',
+                });
+                dataToSend.put(route('users.update', this.userId), {
+                    preserveState: true,
+                    preserveScroll: true,
+                    onSuccess: (page) => {
+                        console.log(page.props.updateStatus);
+                    },
+                    onError: (errors) => {
+                        console.log('Error: ', errors);
+                    }
+                });
+            }
+        },
+        computed: {
+            getGender() {
+                return this.capitalizeFirstLetter(this.gender);
+            }
         }
     }
 </script>
