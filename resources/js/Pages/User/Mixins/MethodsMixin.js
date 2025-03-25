8186ed96f6018b5302d81c5f6fc8a932fbb57017
Nna-ayua Okafor <nna_okafor@yahoo.com>
2025-03-25 07:37:17 +0000

    Created User/Mixins folder

---
+++ resources/js/Pages/User/Mixins/MethodsMixin.js
@@ -0,0 +1,18 @@
+export default {
+    data() {
+        return {
+            adImages: ['photoSample', 'photoSample1', 'photoSample2', 'photoSample3', 'photoSample4', 'photoSample5', 'photoSample6', 'photoSample7', 'photoSample8', 'photoSample9', 'photoSample10', 'photoSample11', 'photoSample12', 'photoSample13', 'photoSample14', 'photoSample15', 'photoSample16', 'photoSample17', 'photoSample18', 'photoSample19', 'photoSample20'],
+        }
+    },
+    methods: {
+        imagePath(index) {
+            const imageName = this.adImages[index];
+            const imageUrl = new URL(`../../../../Images/${imageName}.jpg`, import.meta.url).href;
+
+            return imageUrl;
+        },
+        capitalizeFirstLetter(str) {
+            return str.charAt(0).toUpperCase() + str.slice(1);
+        }
+    }
+}
