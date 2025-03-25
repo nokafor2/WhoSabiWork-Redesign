444dd8d839e685cc0a5714e6ed0b3c5e5b340a03
Nna-ayua Okafor <nna_okafor@yahoo.com>
2025-03-25 07:26:26 +0000

    Updated FirstNameSection.vue

--- resources/js/Pages/User/Components/FirstNameSection.vue
+++ resources/js/Pages/User/Components/FirstNameSection.vue
@@ -4,22 +4,20 @@
             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                 <dl class="row col-12 mb-0">
                     <dt class="col-sm-3">First Name:</dt>
-                    <dd class="col-sm-9 mb-0">Nna-ayua</dd>
+                    <dd class="col-sm-9 mb-0">{{ getFirstName }}</dd>
                 </dl>
             </button>
         </h2>
         <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo">
             <div class="accordion-body">
-                <form class=""  action="{{ route('register') }}" method="POST">
-                    <!-- @csrf -->
+                <form class="" @submit.prevent="submitAction">
                     <div class="form-group col-12 mb-3">
-                        <input type="text" name="first_name" value="{{ old('first_name') }}" placeholder="First name" required 
-                        class="form-control {{ $errors->has('first_name') ? 'is-invalid' : '' }}">
+                        <input type="text" name="first_name" value="" placeholder="First name" class="form-control" v-model="firstNameInput">
                     </div>
                     
                     <div class="row justify-content-between">
                         <button type="submit" name="updateFirstName" class="btn btn-danger col-auto">Update</button>
-                        <button type="submit" name="clearFirstName" class="btn btn-danger col-auto">Clear</button>
+                        <!-- <button type="button" name="clearFirstName" class="btn btn-danger col-auto">Clear</button> -->
                     </div>
                 </form>
             </div>
@@ -29,16 +27,42 @@
 
 
 <script>
+    import MethodsMixin from '../Mixins/MethodsMixin.js';
+    import { useForm } from '@inertiajs/vue3';
+
     export default {
-        props: [],
+        mixins: [MethodsMixin],
+        props: ['firstName', 'userId'],
         emits: [],
         data() {
             return {
-
+                firstNameInput: ""
             }
         },
         methods: {
-
-        }
+            submitAction() {
+                console.log(this.firstNameInput);
+                const dataToSend = useForm({
+                    updateVal: this.firstNameInput,
+                    updateField: 'first_name',
+                });
+                // dataToSend.put('/users/'+this.userId);
+                dataToSend.put(route('users.update', this.userId), {
+                    preserveState: true,
+                    preserveScroll: true,
+                    onSuccess: (page) => {
+                        console.log(page.props.updateStatus);
+                    },
+                    onError: (errors) => {
+                        console.log('Error: ', errors);
+                    }
+                });
+            }
+        },
+        computed: {
+            getFirstName() {
+                return this.capitalizeFirstLetter(this.firstName);
+            }
+        },
     }
 </script>
