ee55b2f058656a503146e976fbc3b57f2d520482
Nna-ayua Okafor <nna_okafor@yahoo.com>
2025-03-26 08:12:01 +0000

    Created SparePartSection.vue

---
+++ resources/js/Pages/User/Components/SparePartSection.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="accordion-item">
+        <h2 class="accordion-header" id="headingNineteen">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNineteen" aria-expanded="false" aria-controls="collapseNineteen">
+                <dl class="row col-12 mb-0">
+                    <dt class="col-sm-3">Spare part sellers:</dt>
+                    <dd class="col-sm-9 mb-0">
+                        <ul class="list-inline mb-0">
+                            <li v-for="(sparePart, index) in selectedSpareParts" :key="index" class="list-inline-item">{{ sparePart }},</li>
+                        </ul>
+                    </dd>
+                </dl>
+            </button>
+        </h2>
+        <div id="collapseNineteen" class="accordion-collapse collapse" aria-labelledby="headingNineteen">
+            <div class="accordion-body">
+                <form class="" @submit.prevent="submitAction">
+                    <div class="form-group col-12 mb-3">
+                        <div v-for="(sparePart, index) in allSpareParts" :key="index" class="form-check form-check-inline">
+                            <input class="form-check-input" name="spare_parts[]" type="checkbox" :id="index" :value="index" v-model="sparePartsInput">
+                            <label class="form-check-label" :for="index">{{ sparePart }}</label>
+                        </div>
+                    </div>
+                    
+                    <div class="row justify-content-between">
+                        <button type="submit" name="updateSpareParts" class="btn btn-danger col-auto">Update</button>
+                        <!-- <button type="submit" name="clearSpareParts" class="btn btn-danger col-auto">Clear</button> -->
+                    </div>
+                </form>   
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+    import MethodsMixin from '../Mixins/MethodsMixin.js';
+    import { useForm } from '@inertiajs/vue3';
+
+    export default {
+        mixins: [MethodsMixin],
+        props: ['allSpareParts', 'selectedSpareParts', 'userId'],
+        emits: [],
+        data() {
+            return {
+                sparePartsInput: []
+            }
+        },
+        methods: {
+            submitAction() {
+                const dataToSend = useForm({
+                    updateVal: this.sparePartsInput,
+                });
+                dataToSend.put(route('technicalServices.update', this.userId), {
+                    preserveState: true,
+                    preserveScroll: true,
+                    onSuccess: (page) => {
+                        console.log(page);
+                    },
+                    onError: (errors) => {
+                        console.log('Error: ', errors);
+                    }
+                });
+            }
+        },
+        computed: {
+            
+        },
+    }
+</script>
