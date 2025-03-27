f4cd0f9b0c179f951fe580080e3d55cce309b077
Nna-ayua Okafor <nna_okafor@yahoo.com>
2025-03-27 16:07:37 +0000

    Created CarBrandController.php

---
+++ app/Http/Controllers/CarBrandController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Traits\GlobalFunctions;
+use App\Models\CarBrand;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class CarBrandController extends Controller
+{
+    use GlobalFunctions;
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $user = User::find($id);
+        $carBrandUser = CarBrand::where('user_id', '=', $user->id)->first();
+        $carBrandObj = new CarBrand();
+        $allCarBrands = $this->getTableColumnsWithSort($carBrandObj->table, CarBrand::$columnsToExclude);
+        $selectedCarBrands = $request->updateVal;
+        foreach ($allCarBrands as $index => $carBrand) {
+            if (in_array($index, $selectedCarBrands)) {
+                $carBrandUser->{$index} = true;
+            } else {
+                $carBrandUser->{$index} = false;
+            }
+        }
+
+        $result = $carBrandUser->save();
+
+        return redirect()->route('users.show', $id)->with('success', $result);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
