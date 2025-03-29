8527f2edc39229e198ccaa989550ccb5bec37e89
Nna-ayua Okafor <nna_okafor@yahoo.com>
2025-03-29 09:22:55 +0000

    Created UserRatingFactory.php

---
+++ database/factories/UsersRatingFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Models\UsersRating;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\UsersRating>
+ */
+class UsersRatingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        // Get the array of business users
+        $businessUsers = User::where('account_type', '=', 'business')->get();
+
+        return [
+            'user_id' => $businessUsers->random()->id,
+            'rating' => $this->faker->numberBetween(1,5),
+            'rated_by_user' => $businessUsers->random()->id,
+        ];
+    }
+}
