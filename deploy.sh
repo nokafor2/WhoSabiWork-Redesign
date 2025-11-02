446586164805e9e71c160a6a8357a0c3c3e8cd1a
Nna-ayua Okafor <nna_okafor@yahoo.com>
2025-11-02 17:20:31 +0000

    Deleted unwanted files and set a reference point for editing getUserDetails() method

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+git add .;
+git commit -m "Implementing display of entrepreneur comments and replies in business account profile. Adding replies to comments in business account profile.";
+git push origin main;
+git push gitlab main;
